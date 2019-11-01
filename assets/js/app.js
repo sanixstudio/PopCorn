1ca5ccb4610c0698845b5011214b0df638f96da7
hatout01 <hatout1@gmail.com>
2019-10-31 17:50:18 -0700

    added data urls (latest - now playing - popular - toprated - upcoming )

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,19 @@
 $(document).ready(function () {
+    const apiKey = 'f3f124a7e3af05d748ddcefe10f25cb0';
+
+    //  latest url
+    let latestUrl = 'https://api.themoviedb.org/3/movie/latest?api_key=' + apiKey + '&language=en-US';
+    // Upcoming url
+    let upcomingUrl = 'https://api.themoviedb.org/3/movie/upcoming?api_key=' + apiKey + '&language=en-US&page=10';
+    // top rated
+    let topRatedUrl = 'https://api.themoviedb.org/3/movie/top_rated?api_key=' + apiKey + '&language=en-US&page=1';
+    // popular
+    let popularUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=' + apiKey + '&language=en-US&page=1';
+    // now playing
+    let nowPlayingUrl = 'https://api.themoviedb.org/3/movie/now_playing?api_key=' + apiKey + '&language=en-US&page=1';
 
     $.ajax({
-        url: 'https://api.themoviedb.org/3/movie/550?api_key=f3f124a7e3af05d748ddcefe10f25cb0',
+        url: 'https://api.themoviedb.org/3/movie/555?api_key=' + apiKey,
         method: 'GET',
         dataType: 'json'
     }).then(data => {
@@ -10,3 +22,5 @@ $(document).ready(function () {
 
     });
 })
+
+
