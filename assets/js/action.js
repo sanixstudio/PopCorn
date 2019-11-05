4a2c81d043f8c8720762fa609bf0b05c47856452
Adnan Niaz <adnanniaz77@yahoo.com>
2019-11-05 04:58:11 -0800

    fixed and syled the action genre

--- assets/js/action.js
+++ assets/js/action.js
@@ -11,9 +11,7 @@ $(document).ready(function () {
         method: 'GET'
 
     }).then(function (_data) {
-
         let results = _data.results;
-
         genreId = getGenreId();
 
         function getGenreId() {
@@ -22,12 +20,14 @@ $(document).ready(function () {
                 // console.log(each);
 
                 if (idsArray.includes(28)) {
-                    $('.action-movies').append(`<div class="row p-2 text-light">
-                            Original Title: ${each.original_title}<br>
-                            Popularity: ${each.popularity}<br>
-                            Votes Count: ${each.vote_count}<br>
-                            Average Votes: ${each.vote_average}<br>
-                            Overview: ${each.overview};
+                    $('.action-movies').append(`<div class="results-container text-light">
+                            <div class="float-left"><img src="${imgDb + each.poster_path}"></div>
+                            <div class="m-title  movie-title">${each.original_title}</div>
+                            <div class="m-title movie-popularity">${each.popularity}</div>
+                            <div class="m-title movie-vote-count">${each.vote_count}</div>
+                            <div class="m-title movie-vote-avg">${each.vote_average}</div>
+                            <div class="m-title movie-release-year">${each.release_date}</div>
+                            <div class="m-title movie-overview">${each.overview}</div>
                         </div>
                         `);
                 }
