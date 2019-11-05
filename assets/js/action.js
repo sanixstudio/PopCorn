cf95fbac9f6a257c1365d5fca2b80b2c80f503bd
hatout01 <hatout1@gmail.com>
2019-11-04 19:05:20 -0800

    pull from master

---
+++ assets/js/action.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+    const apiKey = "f3f124a7e3af05d748ddcefe10f25cb0";
+    const imgDb = "http://image.tmdb.org/t/p/w185/";
+    const genreUrl = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-US&query=action&page=1&include_adult=false`;
+    var genreId;
+    
+    $.ajax({
+
+        url: genreUrl,
+        method: 'GET'
+
+    }).then(function (_data) {
+
+        let results = _data.results;
+    
+        genreId = getGenreId();        
+
+        function getGenreId() {
+            for(let each of results) {
+                for(let key in each) {
+                    let idsArray = each.genre_ids;
+                    // console.log(each);
+
+                    if(idsArray.includes(99)){
+                        $('.action-movies').append(`<div class="row p-2 text-light">
+                            Original Title: ${each.original_title}<br>
+                            Popularity: ${each.popularity}<br>
+                            Votes Count: ${each.vote_count}<br>
+                            Average Votes: ${each.vote_average}<br>
+                            Overview: ${each.overview};
+                        </div>
+                        `);
+                    }
+                }
+            }
+        }
+    });
+
+
+});
